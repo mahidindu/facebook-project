1adb8fb16b05ec71c4850033fc9a02a7f67e59be
Mahi <Mahi@DESKTOP-4QBB53S>
2023-06-08 15:38:07 +0530

    initial commit

--- src/test/java/objects/Facebook_Candidate_Name.java
+++ src/test/java/objects/Facebook_Candidate_Name.java
@@ -1,5 +1,32 @@
 package objects;
 
 public class Facebook_Candidate_Name {
+	WebDriver driver;
+	
+	public FacebookPage(WebDriver driver) {
+		this.driver = driver;
+		// driver = d;
+		 PageFactory.initElements(driver,this);
+		}
+	
+	By Username_editbox = By.cssSelector("#email");;
+	By Password_editbox = By.xpath("//input[@id='pass']");
+	By Login_btn = By.name("login");
 
+	public void EnterUsername() {
+		driver.findElement(Username_editbox).clear();
+		driver.findElement(Username_editbox).sendKeys("Testuser");
+	}
+
+	public void EnterPassword() {
+		driver.findElement(Password_editbox).clear();
+		driver.findElement(Password_editbox).sendKeys("Tester123");
+	}
+
+	public void VerifyLogin() {
+		driver.findElement(Login_btn).click();
+		String title = driver.getTitle();
+		assertEquals(title, "Log in to Facebook");
+
+	}
 }
